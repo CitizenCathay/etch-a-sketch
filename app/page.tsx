003bfd381c84765d6bdd429cab7860f758f458b3
CitizenCathay <89077454+CitizenCathay@users.noreply.github.com>
2023-12-09 01:42:09 +0800

    Adjust footer styles, begin work on grid component, modify page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Grid from "@/components/Grid";
+
 export default function Home() {
-  return <h1 className="font-bold text-teal-500 text-2xl">Hello World</h1>;
+  return <div></div>;
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return <div className="mb-1.4">Footer</div>;
 };
 
 export default Footer;
