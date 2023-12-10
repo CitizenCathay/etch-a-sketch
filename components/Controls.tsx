510977ad34e25151333917b166fed6cc7fda93c8
CitizenCathay <89077454+CitizenCathay@users.noreply.github.com>
2023-12-10 22:00:42 +0800

    Implement hook for color input and drawing logic for selected color

--- components/Controls.tsx
+++ components/Controls.tsx
@@ -3,23 +3,44 @@ import Grid from "./Grid";
 
 const Controls = () => {
   const [gridSize, setGridSize] = React.useState<number>(16);
-
   const gridSizeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = parseInt(event.target.value, 10); // Extract the new value from the input
     setGridSize(newValue);
   };
   /* console.log(gridSize); */
+
+  const [selectedColor, setSelectedColor] = useState("#000000"); // Initial color value
+  const handleColorChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedColor(event.target.value); // Update the selected color state
+  };
+
   return (
-    <div className="gridControls flex	justify-center items-center	flex-col lg:flex-row mb-6">
-      <Grid gridSize={gridSize} />
-      <div className="gridControls lg:ml-4 flex flex-col space-y-6 mt-6 sm:mt-5">
-        <button className="border rounded border-black ">Color Mode</button>
-        <button className="border rounded border-black">Rainbow Mode</button>
-        <button className="border rounded border-black">Eraser</button>
-        <button className="border rounded border-black">Clear</button>
-        <button className="border rounded border-black ">Toggle Cells</button>
+    <div className="gridControls flex	justify-center items-center	flex-col xl:flex-row mb-6">
+      <Grid gridSize={gridSize} selectedColor={selectedColor} />
+      <div className="gridControls xl:ml-12 flex flex-col space-y-6 mt-6 sm:mt-5">
+        <input
+          type="color"
+          className="w-44 h-12 appearance-none bg-transparent p-0 rounded-lg border-none"
+          value={selectedColor}
+          onChange={handleColorChange}
+        ></input>
+        <button className="border rounded border-black lg:text-lg px-6 py-1 shadow-lg">
+          Color Mode
+        </button>
+        <button className="border rounded border-black  lg:text-lg px-6 py-1 shadow-lg">
+          Rainbow Mode
+        </button>
+        <button className="border rounded border-black  lg:text-lg px-6 py-1 shadow-lg">
+          Eraser
+        </button>
+        <button className="border rounded border-black  lg:text-lg px-6 py-1 shadow-lg">
+          Clear
+        </button>
+        <button className="border rounded border-black  lg:text-lg px-6 py-1 shadow-lg">
+          Toggle Cells
+        </button>
         <div className="sizeControl">
-          <p className="select-none	">
+          <p className="select-none	lg:text-lg">
             Grid Size: {gridSize} × {gridSize}
           </p>
           <input
@@ -29,7 +50,7 @@ const Controls = () => {
             value={gridSize}
             onChange={gridSizeChange}
             id="gridSizeRange"
-            className="" /* For future styling */
+            className="w-44" /* For future styling */
           />
         </div>
       </div>
--- components/Grid.tsx
+++ components/Grid.tsx
@@ -2,36 +2,54 @@ import React, { useEffect, useRef, useState } from "react";
 
 interface GridProps {
   gridSize: number;
+  selectedColor: string;
 }
 
-const Grid: React.FC<GridProps> = ({ gridSize }) => {
+const Grid: React.FC<GridProps> = ({ gridSize, selectedColor }) => {
   // Import gridSize value from Controls parent component
 
   useEffect(() => {
-    createGrid(gridSize); // Initial grid size
-  }, [gridSize]);
+    createGrid(gridSize, selectedColor); // Initial grid size
+  }, [gridSize, selectedColor]);
 
-  const createGrid = (gridSize: number) => {
+  const createGrid = (gridSize: number, selectedColor: string) => {
     const gridContainer = document.querySelector(
       ".gridContainer"
     ) as HTMLDivElement | null;
     if (gridContainer) {
       gridContainer.innerHTML = ""; // Clear previous content
-
       gridContainer.style.display = "grid";
       gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
       gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 
+      let isMouseDown = false;
+      const handleMouseDown = () => {
+        isMouseDown = true;
+      };
+      const handleMouseUp = () => {
+        isMouseDown = false;
+      };
+      const handleMouseInteraction = (event: MouseEvent) => {
+        if (isMouseDown) {
+          const target = event.target as HTMLDivElement | null;
+          if (target && "classList" in target) {
+            target.style.backgroundColor = selectedColor;
+          }
+        }
+      };
       for (let i = 0; i < gridSize * gridSize; i++) {
         const gridCell = document.createElement("div");
-        gridCell.className = "grid-cell border-2 border-red-500";
+        gridCell.className = "grid-cell select-none border-2 border-red-500";
+        gridCell.addEventListener("mousedown", handleMouseDown);
+        gridCell.addEventListener("mouseup", handleMouseUp);
+        gridCell.addEventListener("mousemove", handleMouseInteraction);
         gridContainer.appendChild(gridCell);
       }
     }
   };
 
   return (
-    <div className="gridComponent mt-1 lg:ml-36">
+    <div className="gridComponent mt-1 xl:ml-56">
       <div className="gridContainer w-96 h-96 md:w-[39rem] md:h-[39rem] sm:h-[30rem] sm:w-[30rem] border-2 border-red-500 "></div>
     </div>
   );
