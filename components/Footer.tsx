1a9cd6ab8c87486b19e44ad20d945508f7e904ba
CitizenCathay <89077454+CitizenCathay@users.noreply.github.com>
2024-02-26 17:38:25 +0800

    Update footer with current year and link to github profile

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 
 const Footer = () => {
+  const getYear = () => {
+    return new Date().getFullYear();
+  };
   return (
     <footer className="mb-1.4 w-screen flex justify-center text-xl md:text-2xl select-none mt-4">
-      Footer
+      © {getYear()}
+      <p>
+        &nbsp; <a href="https://github.com/CitizenCathay">Choi Meng Yew</a>
+      </p>
     </footer>
   );
 };
